b5bf170ef4f675fbc50c5ab5c97ad22946916010
Denis Atkesone <denis.atkesone@gmail.com>
2017-10-31 14:25:28 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,10 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
+function onKeyDownHandler(e) {
+}
+  
 function init() {
   // Write your JavaScript code inside the init() function
-
+  document.addEvenListener('keydown', onKeyDownHandler(event));
 }
+
